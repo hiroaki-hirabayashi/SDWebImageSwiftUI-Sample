075be8b47f8147974f651505fec344afa19adfd9
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-01-20 23:34:00 +0900

    Initial Commit

---
+++ SDWebImageSwiftUI-Sample/SDWebImageSwiftUI_SampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  SDWebImageSwiftUI_SampleApp.swift
+//  SDWebImageSwiftUI-Sample
+//
+//  Created by Hiroaki-Hirabayashi on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct SDWebImageSwiftUI_SampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
